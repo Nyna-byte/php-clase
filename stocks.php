ae93dfc200b0c190cd8124a4ad5c4e08c160fd7c
Nyna <45694364+Nyna-byte@users.noreply.github.com>
2021-10-05 09:39:07 +0200

    Update stocks.php
    Stocks pero con arrays indexados para guardar los asociativos

--- stocks.php
+++ stocks.php
@@ -6,44 +6,19 @@
 	<?php
 
 		for($i=1; $i<=35; $i++){
+			${"ib".$i}['nombre']="acciona";
 			${"ib".$i}['precio']=31.730;
 			${"ib".$i}['valoracion']=-0.28;
 			${"ib".$i}['variacion']=6;
 			${"ib".$i}['volumen']=546;
-			${"ib".$i}['hora']=21:29;
+			${"ib".$i}['hora']=21.29;
 		}
 
-		/*
-		$ib1['precio']=31.730;
-		$ib1['valoracion']=-0.28;
-		$ib1['variacion']=6;
-		$ib1['volumen']=546;
-		$ib1['hora']=21:29;
+		for($i=1; $i<=35; $i++){
+			$ibex35[$i-1]=${"ib".$i};
+			var_dump($ibex35[$i-1]); //muestra el contenido de todos los arrays pero para buscarlos hay que acceder y recorrer el array de $ibex35
+		}
 
-		$ib2['precio']=31.730;
-		$ib2['valoracion']=-0.28;
-		$ib2['variacion']=6;
-		$ib2['volumen']=546;
-		$ib2['hora']=21:29;
-
-		$ib3['precio']=31.730;
-		$ib3['valoracion']=-0.28;
-		$ib3['variacion']=6;
-		$ib3['volumen']=546;
-		$ib3['hora']=21:29;
-		*/
-
-
-		$ibex35['acciona']=$ib1;
-		$ibex35['xd']=$ib2;
-		$ibex35['dfg']=$ib3;
-		$ibex35['triyu']=$ib4;
-		$ibex35['xvx']=$ib5;
-		$ibex35['weq']=$ib6;
-		$ibex35['hjwf']=$ib7;
-		$ibex35['rtjds']=$ib8;
-		$ibex35['baej']=$ib9;
-		$ibex35['ctukm']=$ib10; //queda guardado pero no hay forma de recorrerlo
 
 	?>
 </body>
